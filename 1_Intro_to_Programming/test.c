48d424f8d828bb741b8ce299e62d88aea3c97d50
sultanul-ovi <sultanulislamovi@gmail.com>
2023-10-27 22:21:40 -0400

    module pointers

--- 1_Intro_to_Programming/test.c
+++ 1_Intro_to_Programming/test.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-int main()
-{
-    int i = 0;
-    while (i = 0) {
-        printf("Hello\n");
+    int main()
+    {
+        const int ary[4] = {1, 2, 3, 4};
+        int *p;
+        p = ary + 3;
+        *p = 5;
+        printf("%d\n", ary[3]);
     }
-    return 0;
-}
