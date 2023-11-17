6101ff4f613544541e826b6b5f7de3a2d052089b
sultanul-ovi <sultanulislamovi@gmail.com>
2023-11-16 22:11:20 -0500

    module 3

--- ovi.cpp
+++ ovi.cpp
@@ -1,6 +1,59 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int main() {
+    // Fast I/O in C++
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    // Example output
+    cout << "Hello, World!" << endl;
+
+    // Uncomment the sections below as needed
+
+    // // Input section
+    // int n;
+    // char ch;
+    // string str;
+    // cin >> n >> ch; // Basic input
+    // getline(cin, str); // String input
+
+    // // Working with strings
+    // char s[100];
+    // cin.getline(s, 100); // C-style string input
+
+    // // Loops and conditional logic
+    // for(int i = 0; i < n; ++i) {
+    //     // loop body
+    // }
+    // while(cin >> n) {
+    //     // loop that reads until EOF
+    // }
+
+    // // Math operations
+    // int a = 5, b = 10;
+    // swap(a, b);
+    // int min_value = min(a, b);
+    // int max_value = max(a, b);
+    // int array_min = min({a, b, -1, 7});
+    // int array_max = max({a, b, -1, 7});
+
+    // // Output with fixed precision
+    // cout << fixed << setprecision(5) << 123.456789 << endl;
+
+    // // Ternary operator
+    // (n % 2 == 0) ? cout << "Even\n" : cout << "Odd\n";
+
+    // End of program
+    return 0;
+}
+
+// Functions can be defined here
+
+// End of File
+
+
+
 int main()
 {
     cout << "hello" << endl;
