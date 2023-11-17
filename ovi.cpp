bc98561824bd8ba946d3440bd1fab33f6bcefcec
sultanul-ovi <sultanulislamovi@gmail.com>
2023-11-16 20:20:07 -0500

    hi

--- ovi.cpp
+++ ovi.cpp
@@ -1,19 +1,34 @@
 #include <iostream>
 #include <stdio.h>
 #include <iomanip>
+#include <algorithm>
+#include <utility>
+#include <string.h>
+#include <string>
 using namespace std;
 
 int main()
 {
-    int n;
-    char ch;
+    // int n;
+    // char ch;
 
-    // cin >> n;
-    // cout << n;
+    // char s[100];
+    // cin.getline(s, 100);
 
-    cin >> n >> ch;
-    cout << n << " " << ch;
+    // cin >> n >> ch;
+    // cout << n << " " << ch;
+
+    // swap(a, b);
+    // int c = min(a, b);
+    // int d = max(a, b); 
+    // int mn = min({a, b, c, d});
+    // int mx = max({a, b, c, d});       
 }
+
+
+
+
+
 
 // // End of File
 //     int a, b;
@@ -24,3 +39,6 @@ int main()
 
 // // Set Precision
 //     cout << fixed << setprecision(5) << a << endl;
+
+// // Ternary operator
+// (n % 2 == 0) ? cout << "Even" << endl : cout << "Odd" << endl;
