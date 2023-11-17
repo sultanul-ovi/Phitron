b5b85c1213a6d7585dfd2f25536722526d527bf1
sultanul-ovi <sultanulislamovi@gmail.com>
2023-11-16 20:08:18 -0500

    half way

---
+++ ovi.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <stdio.h>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    int n;
+    char ch;
+
+    // cin >> n;
+    // cout << n;
+
+    cin >> n >> ch;
+    cout << n << " " << ch;
+}
+
+// // End of File
+//     int a, b;
+//     while (cin >> a >> b)
+//     {
+//         cout << a << " " << b << endl;
+//     }
+
+// // Set Precision
+//     cout << fixed << setprecision(5) << a << endl;
