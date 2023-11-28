cedcf193e5416e374fc3d0bf3ac737d99fe16d73
sultanul-ovi <sultanulislamovi@gmail.com>
2023-11-28 04:09:46 -0500

    a few new files

--- test.cpp
+++ test.cpp
@@ -1,15 +1,19 @@
-#include <stdio.h>
-void f2()
-{
-    printf("f1 ");
-}
-void f1()
-{
-    printf("f2 ");
-    f2();
-}
+#include<bits/stdc++.h>
+using namespace std;
 int main()
 {
-    printf("Main ");
-    f1();
+    int n; cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++) {
+        cin>>a[i];
+    }
+    for(int i=0;i<n-1;i++) {
+        for(int j=i+1;j<n;j++) {
+            if(a[i]>a[j]) swap(a[i],a[j]);
+        }
+    }
+    for(int i=0;i<n;i++) {
+        cout<<a[i]<<" ";
+    }
+    return 0;
 }
